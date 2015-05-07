a2172b59bdde3811392ab825353ed5a0d6099099
Rakovchu <nikita.rakovchuk@gmail.com>
2015-05-07 21:20:53 +0300

    Add calc entry in proc

---
+++ calc.c
@@ -0,0 +1,53 @@
+#include <linux/module.h>    // included for all kernel modules
+#include <linux/kernel.h>    // included for KERN_INFO
+#include <linux/init.h>      // included for __init and __exit macros
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("/proc calculator module");
+
+static ssize_t read_callback(struct file *file, char *buf, size_t count, loff_t *ppos)
+{
+    char *result_str = "123\n";
+    int len = strlen(result_str);
+    // We only support reading the whole string at once.
+    if (count < len)
+            return -EINVAL;
+    // If file position is non-zero, then assume the string has
+    // been read and indicate there is no more data to be read.
+    if (*ppos != 0)
+            return 0;
+
+    if (copy_to_user(buf, result_str, len))
+            return -EINVAL;
+    // Tell the user how much data we wrote.
+    *ppos = len;
+
+    return len;
+}
+
+static const struct file_operations calc_fops = {
+    .owner = THIS_MODULE,
+    .read  = read_callback,
+};
+
+static int __init calc_init(void)
+{
+    printk(KERN_INFO "Calc module loaded\n");
+    if (!proc_create("calc", 0, NULL, &calc_fops)) {
+        printk(KERN_ERR "cannot create calc entry in /proc\n");
+        return -ENOMEM;
+    }
+
+    return 0;    // Non-zero return means that the module couldn't be loaded.
+}
+
+static void __exit calc_cleanup(void)
+{
+    printk(KERN_INFO "Cleaning up module.\n");
+    remove_proc_entry("calc", NULL);
+}
+
+module_init(calc_init);
+module_exit(calc_cleanup);
